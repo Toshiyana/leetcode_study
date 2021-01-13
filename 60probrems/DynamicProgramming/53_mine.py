85410ec91843ab7d2749ade4a9a08bb25e6f18ee
Toshiyana <toshiyana0306@gmail.com>
2021-01-13 12:40:12 +0900

    add 53 about DP

---
+++ 60probrems/DynamicProgramming/53_mine.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxSubArray(self, nums: [int]) -> int:
+        if not nums:
+            return 0
+
+        curSum = maxSum = nums[0]
+        for num in nums[1:]:
+            print("num: {}, curSum+num: {} + {} = {}".format(num, curSum, num, curSum+num))
+            curSum = max(num, curSum + num)
+            print("curSum:", curSum)
+
+            print("maxSum: {}, curSum: {}".format(maxSum, curSum))
+            maxSum = max(maxSum, curSum)
+            print("maxSum:", maxSum)
+
+            print("")
+
+        return maxSum
+
+
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+
+obj = Solution()
+print(obj.maxSubArray(nums))
