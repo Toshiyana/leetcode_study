c040038691e7cf901b5f049695e46923f5655f66
Toshiyana <toshiyana0306@gmail.com>
2021-01-28 00:03:39 +0900

    add 2 about LinkedList

--- 60probrems/LinkedList/2_mine.py
+++ 60probrems/LinkedList/2_mine.py
@@ -1,7 +1,32 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        list1 = []
+        list2 = []
+
+        tail = l1
+        while tail:
+            list1.append(str(tail.val))
+            tail = tail.next
+
+        tail = l2
+        while tail:
+            list2.append(str(tail.val))
+            tail = tail.next
+
+        num1 = int(''.join(list(reversed(list1))))
+        num2 = int(''.join(list(reversed(list2))))
+
+        ans = str(num1 + num2)
+        ans = list(ans)
+
+        tail = head = ListNode(int(ans.pop(-1))) # intなくても正解だった（time: intなし（str）のときは80ms, intありの時は100ms）
+        while ans:
+            tail.next = ListNode(int(ans.pop(-1)))
+            tail = tail.next
+
+        return head
