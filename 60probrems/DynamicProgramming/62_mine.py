608381acbeea528ecc6ede493c37078f659a688f
Toshiyana <toshiyana0306@gmail.com>
2021-01-30 00:15:24 +0900

    add 62 about DP

--- 60probrems/DynamicProgramming/62_mine.py
+++ 60probrems/DynamicProgramming/62_mine.py
@@ -1,3 +1,14 @@
+import math
+# combination
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        
+        if n__ot m or not n:
+            return 0
+        return self.combinations_count(m-1, n-1)
+
+    def combinations_count(self, a, b):
+        return math.factorial(a+b) // (math.factorial(a) * math.factorial(b))
+
+m, n = 3, 7
+obj = Solution()
+print(obj.uniquePaths(m,n))
---
+++ 60probrems/DynamicProgramming/62_oth.py
@@ -0,0 +1,20 @@
+class Solution:
+    # O(m*n) space
+    def uniquePaths(self, m: int, n: int) -> int:
+        if not m or not n:
+            return 0
+        dp = [[1 for _ in range(n)] for _ in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
+
+    # O(n) space <- fastest
+    def uniquePaths2(self, m, n):
+        if not m or not n:
+            return 0
+        cur = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                cur[j] += cur[j-1]
+        return cur[-1]
